a33b40838fedd19e5958ebeb15b2980238b9c49d
Sam M <74025628+TestAccount3500@users.noreply.github.com>
2020-11-12 23:16:29 -0500

    Added print file

---
+++ PrintStatement.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class PrintStatement {
+  public static void main(String[] args){
+    String statement = "Hello";  //edit text here
+    System.out.println(statement);
+  }
+}
